7be83c64f3658054a000ba8bc9879d1755054f12
“Gunay <gunaygultekin@gmail.com>
2023-10-05 04:43:06 -0400

    Implemented ExpenseItem and ExpenseList components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
 
 import { AppProvider } from './context/AppContext';
 
@@ -23,10 +24,9 @@ const App = () => {
                     <div className="col-sm">
                         <ExpenseTotal />
                     </div>
-                    {
-                        /* Add ExpenseList component here */
-                    }
-
+                    <div className="col-sm">
+                        <ExpenseList />
+                    </div>
                     {
                         /* Add ExpenseItem component here */
                     }
